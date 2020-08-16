057f4d74c61f6c9551c48cfe7a0c893bdd912e41
Aman Irshad <aman.itz.me07@gmail.com>
2020-08-16 23:56:40 +0530

    feat added Authentication using FirebaseUI and added buttons for Phone as well as Google Sign-in issues vector assets for btn_phone_sign_in is not working

--- app/src/main/java/com/amanirshad/uberbyaman/SplashScreenActivity.java
+++ app/src/main/java/com/amanirshad/uberbyaman/SplashScreenActivity.java
@@ -1,10 +1,20 @@
 package com.amanirshad.uberbyaman;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.firebase.ui.auth.AuthMethodPickerLayout;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import io.reactivex.Completable;
@@ -12,16 +22,84 @@ import io.reactivex.android.schedulers.AndroidSchedulers;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
+    private final static int LOGIN_REQUEST_CODE = 7171;
+    private List<AuthUI.IdpConfig> providers;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener listener;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        delaySplashScreen();
+
+    }
+
+    @Override
+    protected void onStop() {
+        if (firebaseAuth != null && listener != null)
+            firebaseAuth.removeAuthStateListener(listener);
+        super.onStop();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        delaySplashScreen();
+        init();
+        // delaySplashScreen();
+    }
+
+    private void init() {
+        providers = Arrays.asList(
+                new AuthUI.IdpConfig.PhoneBuilder().build(),
+                new AuthUI.IdpConfig.GoogleBuilder().build());
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        listener = myFirebaseAuth -> {
+            FirebaseUser user = myFirebaseAuth.getCurrentUser();
+            if (user != null)
+                Toast.makeText(this, "Welcome: " + user.getUid(), Toast.LENGTH_SHORT).show();
+            else
+                showLoginLayout();
+        };
+    }
+
+    private void showLoginLayout() {
+        AuthMethodPickerLayout authMethodPickerLayout = new AuthMethodPickerLayout
+                .Builder(R.layout.layout_sign_in)
+                .setPhoneButtonId(R.id.btn_phone_sign_in)
+                .setGoogleButtonId(R.id.btn_google_sign_in)
+                .build();
+
+        startActivityForResult(AuthUI.getInstance().
+                createSignInIntentBuilder()
+                .setAuthMethodPickerLayout(authMethodPickerLayout)
+                .setIsSmartLockEnabled(false).setTheme(R.style.LoginTheme)
+                .setAvailableProviders(providers)
+                .build(), LOGIN_REQUEST_CODE);
     }
 
     private void delaySplashScreen() {
-        Completable.timer(5, TimeUnit.SECONDS,
+        Completable.timer(3, TimeUnit.SECONDS,
                 AndroidSchedulers.mainThread())
-                .subscribe(() -> Toast.makeText(SplashScreenActivity.this, "Splash Screen done!!...", Toast.LENGTH_SHORT).show());
+                .subscribe(() ->
+
+                        //After Splash screen , ask login if not login
+                        firebaseAuth.addAuthStateListener(listener)
+                );
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == LOGIN_REQUEST_CODE) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+            if (resultCode == RESULT_OK) {
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+            } else {
+                Toast.makeText(this, "[ERROR]: " + response.getError().getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
